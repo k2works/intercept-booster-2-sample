27c0a5b514f8c353cb692d425c959dcd5e65ef18
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-10-19 10:00:40 +0900

    build:セットアップ

---
+++ gulpfile.cjs
@@ -0,0 +1,29 @@
+const { series, parallel } = require('gulp');
+const core = require('./ops/gulp/tasks/core');
+
+exports.default = series(
+    core.webpackBuildTasks(),
+    parallel(
+        core.asciidoctorBuildTasks(),
+        core.marpBuildTasks(),
+    ),
+    series(
+        parallel(core.webpack.server, core.asciidoctor.server),
+        parallel(core.webpack.watch, core.asciidoctor.watch, core.marp.watch),
+    ),
+);
+
+exports.build = series(
+    core.webpackBuildTasks(),
+    parallel(
+        core.asciidoctorBuildTasks(),
+        core.marpBuildTasks(),
+    )
+);
+
+exports.docs = series(
+    parallel(core.asciidoctorBuildTasks(), core.marpBuildTasks()),
+    parallel(core.asciidoctor.server, core.asciidoctor.watch, core.marp.watch),
+);
+exports.slides = series(core.marp.build);
+
